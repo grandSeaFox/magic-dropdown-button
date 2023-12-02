1b5ccc35bd5414d872db98316f8c4a8062df2686
Gonçalo Raposeiro <goncalo.raposeiro@outlook.com>
2023-12-02 03:20:23 +0000

    First version of the button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css';
 
-export default function MagicDropdownButton() {
-    return <button>Hello 1</button>
-}
+export default function MagicDropdownButton({ options }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="dropdown">
+            <button className="dropdown-button" onClick={toggleDropdown}>
+                {selectedOption || 'Select an option'}
+            </button>
+            {isOpen && (
+                <ul className="dropdown-options">
+                    {options.map((option) => (
+                        <li key={option} onClick={() => handleOptionClick(option)}>
+                            {option}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
 
 module.exports = MagicDropdownButton
